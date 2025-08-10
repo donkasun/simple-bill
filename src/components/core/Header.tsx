9ac8f80c804230faa2dd1ab00b2b915a737e44e5
Don Kasun Gallage <kasungallage94@gmail.com>
2025-08-10 11:58:37 +0530

    feat(styles): update CSS variables for avatar and button sizes; enhance icon-button and avatar styles in Header component

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -55,11 +55,6 @@ const Header: React.FC<HeaderProps> = () => {
         </nav>
 
         <div className="header-actions">
-          <button className="icon-button" aria-label="Notifications">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden>
-              <path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-          </button>
           <div className="user-menu" ref={menuRef}>
             <button
               className="avatar-button"
